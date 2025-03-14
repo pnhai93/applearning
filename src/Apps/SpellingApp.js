7eca66be5aab9da745cd53b59ed1db17355a952a
pnhai93 <phamngochai193@gmail.com>
2025-03-14 15:15:37 +0700

    Update SpellingApp.js

--- src/Apps/SpellingApp.js
+++ src/Apps/SpellingApp.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 export default function SpellingGame() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-blue-100">
-      <h1 className="text-3xl font-bold mb-4">T?p ?�nh V?n</h1>
+      <h1 className="text-3xl font-bold mb-4">chua xong</h1>
       
     </div>
   );
